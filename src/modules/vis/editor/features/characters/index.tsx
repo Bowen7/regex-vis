ecd03d1c6a640f242647d514cabac7573a08fc34
张文翔 <771797746@qq.com>
2021-03-01 23:10:29 +0800

    conflict

--- src/modules/vis/editor/features/characters/index.tsx
+++ src/modules/vis/editor/features/characters/index.tsx
@@ -1,13 +1,52 @@
-import React, { useState } from "react"
-import { Input, Spacer } from "@geist-ui/react"
+import React, { useState, useContext, useEffect } from "react"
+import { Input, Spacer, Button } from "@geist-ui/react"
 import RadioGroup from "@/components/radioGroup"
 import Cell from "@/components/cell"
 import { charactersOptions } from "./helper"
-import { Character } from "@/types"
+import { Character, StringCharacter } from "@/types"
+import VisContext from "../../../context"
+import { ActionTypes } from "@/reducers/vis"
 type Prop = {
   character: Character
 }
 const Characters: React.FC<Prop> = ({ character }) => {
+  const { dispatch } = useContext(VisContext)
+  const [edited, setEdited] = useState(false)
+  const [stringValue, setStringValue] = useState("")
+
+  useEffect(() => {
+    if (!edited) {
+      switch (character.type) {
+        case "string": {
+          setStringValue(character.value)
+          break
+        }
+      }
+    }
+  }, [character, edited])
+
+  const handleStringValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value !== stringValue) {
+      setEdited(true)
+    }
+    setStringValue(e.target.value)
+  }
+
+  const handleApply = () => {
+    let val!: StringCharacter
+    switch (character.type) {
+      case "string": {
+        val = { ...character, value: stringValue }
+        break
+      }
+    }
+    dispatch({
+      type: ActionTypes.EDIT_CHARACTER,
+      payload: {
+        val,
+      },
+    })
+  }
   return (
     <>
       <Cell label="Characters:">
@@ -18,7 +57,17 @@ const Characters: React.FC<Prop> = ({ character }) => {
         />
         <Spacer y={0.5} />
         {character.type === "string" && (
-          <Input size="small" value={character.value} />
+          <>
+            <Input
+              size="small"
+              value={stringValue}
+              onChange={handleStringValueChange}
+            />
+            <Spacer inline />
+            <Button auto size="small" onClick={handleApply}>
+              Apply
+            </Button>
+          </>
         )}
       </Cell>
       <style jsx>{``}</style>
