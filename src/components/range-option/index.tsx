5bf07c5fa64119e5050a82f0aca97563ddf8592d
Bowen7 <771797746@qq.com>
2021-03-17 19:18:24 +0800

    range option

--- src/components/range-option/index.tsx
+++ src/components/range-option/index.tsx
@@ -1,22 +1,37 @@
-import React from "react"
-import { Code, AutoComplete } from "@geist-ui/react"
+import React, { useState, useRef } from "react"
+import { AutoComplete, Spacer, useClickAway, useTheme } from "@geist-ui/react"
+import { CheckInCircle, Trash2 } from '@geist-ui/react-icons'
 import { Range } from "@/types"
 type Prop = {
   range: Range
 }
 const RangeOption: React.FC<Prop> = ({ range }) => {
   const { from, to } = range
+  const [focused, setFocused] = useState(false)
+  const wrapRef = useRef<HTMLDivElement>(null)
+
+  useClickAway(wrapRef, () => setFocused(false))
+  const handleWrapperClick = () => setFocused(true)
+
+  const theme = useTheme()
+  console.log(theme)
   return (
     <>
-      <div className="wrapper">
+      <div ref={wrapRef} className="wrapper" onClick={handleWrapperClick}>
         <AutoComplete value={from} size="small" />
         {" - "}
         <AutoComplete value={to} size="small" />
+        {focused && <span className="operations">
+          <CheckInCircle size={18} color="#333"/>
+          <Spacer inline x={0.5}/>
+          <Trash2 size={18} color="#333"/>
+        </span>}
       </div>
       <style jsx>{`
         .wrapper {
+          position: relative;
           display: inline-block;
-          border: 1px solid rgb(234, 234, 234);
+          border: 1px solid ${focused ? '#0070F3' :'rgb(234, 234, 234)'};
           border-radius: 5px;
         }
         .wrapper :global(.auto-complete) {
@@ -29,6 +44,21 @@ const RangeOption: React.FC<Prop> = ({ range }) => {
         }
         .wrapper :global(input) {
           text-align: center;
+        }
+
+        .operations {
+          position: absolute;
+          right: 0;
+          top: 0;
+          transform: translate(100%, 0);
+          display: flex;
+          align-items: center;
+          height: 32px;
+          padding: 0 12px;
+        }
+
+        .operations :global(svg) {
+          cursor: pointer;
         }
       `}</style>
     </>
