19dc647c334b4b0303d33d288cb0fd0b268f331d
张文翔 <771797746@qq.com>
2021-04-20 00:22:31 +0800

    tree shaking

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,17 @@
-const { override, addWebpackAlias, addBabelPlugin } = require("customize-cra")
+const {
+  override,
+  addWebpackAlias,
+  addBabelPlugin,
+  fixBabelImports,
+} = require("customize-cra")
 const path = require("path")
 
 module.exports = override(
   addWebpackAlias({
     "@": path.resolve(__dirname, "src"),
   }),
-  addBabelPlugin("styled-jsx/babel")
+  addBabelPlugin("styled-jsx/babel"),
+  fixBabelImports("@geist-ui/react", {
+    libraryDirectory: "esm",
+  })
 )
