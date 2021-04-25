75a666b0770b1c4c9d2ca749e23c83956792fadf
张文翔 <771797746@qq.com>
2021-04-25 23:48:36 +0800

    quantifier

--- src/modules/editor/features/quantifier/index.tsx
+++ src/modules/editor/features/quantifier/index.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react"
-import { Checkbox } from "@geist-ui/react"
+import { Checkbox, Select } from "@geist-ui/react"
 import Cell from "@/components/cell"
-import RadioGroup from "@/components/radio-group"
 import { quantifierOptions } from "./helper"
 const Quantifier: React.FC<{}> = () => {
   const [times, setTimes] = useState("non")
   return (
     <>
-      <Cell label="Quantifier times">
-        <RadioGroup
-          options={quantifierOptions}
-          value={times}
-          onChange={setTimes}
-        />
-      </Cell>
-      <Cell label="Quantifier greedy">
-        <Checkbox initialChecked={true}>Greedy</Checkbox>
+      <Cell label="Quantifier">
+        <Cell.Item label="times">
+          <Select
+            value={times}
+            onChange={(value) => setTimes(value as string)}
+            getPopupContainer={() => document.getElementById("editor-content")}
+            disableMatchWidth
+          >
+            {quantifierOptions.map(({ value, label }) => (
+              <Select.Option value={value} key={value}>
+                <span>{label}</span>
+              </Select.Option>
+            ))}
+          </Select>
+        </Cell.Item>
+        <Cell.Item label="greedy">
+          <Checkbox initialChecked={true}>Greedy</Checkbox>
+        </Cell.Item>
       </Cell>
     </>
   )
