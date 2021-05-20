9b135e344c3ecded5f0301b62cbe92a87cd31fbc
张文翔 <771797746@qq.com>
2021-05-20 23:45:44 +0800

    feat: export legend svg

--- scripts/export.js
+++ scripts/export.js
@@ -1,8 +1,24 @@
 const React = require("react")
 const ReactDOMServer = require("react-dom/server")
-const Header = require("./dest/index.js")
-const string = ReactDOMServer.renderToString(
-  React.createElement(Header, { regex: `/abc/` })
-).replace(/ class="[\w-]+"/g, "")
+const fs = require("fs")
+const path = require("path")
+const MinimumRailroad = require("../dest/index.js")
+const exportList = [
+  {
+    name: "string",
+    regex: "/a/",
+    selected: false,
+  },
+]
+const classRegexp = / class="[\w-]+"/g
 
-console.log(string)
+exportList.forEach(({ name, regex, select }) => {
+  const string = ReactDOMServer.renderToString(
+    React.createElement(MinimumRailroad, { regex, select })
+  ).replace(classRegexp, "")
+
+  fs.writeFileSync(
+    path.resolve(__dirname, "../src/assets/" + name + ".svg"),
+    string
+  )
+})
--- src/modules/railroad/minimum-railroad.tsx
+++ src/modules/railroad/minimum-railroad.tsx
@@ -12,9 +12,11 @@ type Props = {
 const MinimumRailroad: React.FC<Props> = ({ regex, selected = false }) => {
   const rootRenderNode = traverse.render(parser.parse(regex))
 
-  const selectedIds = rootRenderNode.children
-    .filter((child) => child.type === "node")
-    .map((child) => (child as RenderNode).id)
+  const selectedIds = selected
+    ? rootRenderNode.children
+        .filter((child) => child.type === "node")
+        .map((child) => (child as RenderNode).id)
+    : []
   return (
     <SvgContainer
       rootRenderNode={rootRenderNode}
