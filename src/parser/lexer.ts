794602ce372ca889f39b13ccbcffceb0f0b0650c
张文翔 <771797746@qq.com>
2022-01-23 23:50:12 +0800

    refactor[wip]: parser

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -1,4 +1,87 @@
+import * as AST from "./ast"
+type Span = {
+  start: number
+  end: number
+}
+export enum TokenType {
+  RegexBodyStart,
+  RegexBodyEnd,
+  Literal,
+  GroupStart,
+  GraphEnd,
+  RangeStart,
+  RangeEnd,
+  Choice,
+}
+export type Token = {
+  type: TokenType
+  span: Span
+}
+const tokenRegex = /[/()[|]/
+const tokenMap = new Map(
+  Object.entries({
+    "(": TokenType.GroupStart,
+    ")": TokenType.GraphEnd,
+    "[": TokenType.RangeStart,
+    "|": TokenType.Choice,
+  })
+)
 class Lexer {
-  constructor() {}
+  regex: string
+  index = 0
+  constructor(regex: string) {
+    this.regex = regex
+  }
+
+  public read(): Token {
+    const start = this.index
+    while (!tokenRegex.test(this.regex[this.index])) {
+      this.advance(1)
+    }
+    if (start !== this.index) {
+      return {
+        type: TokenType.Literal,
+        span: { start, end: this.index },
+      }
+    }
+    const char = this.regex[this.index]
+    switch (char) {
+      case "/": {
+        return this.index === 0
+          ? this.token(TokenType.RegexBodyStart)
+          : this.token(TokenType.RegexBodyEnd)
+      }
+      default: {
+        return this.token(tokenMap.get(char)!)
+      }
+    }
+  }
+
+  token(type: TokenType, size = 1): Token {
+    const span = { start: this.index, end: this.advance(size) }
+    return {
+      type,
+      span,
+    }
+  }
+
+  public readTarget(target: string): boolean {
+    const char = this.regex[this.index]
+    if (target === char) {
+      this.advance(1)
+      return true
+    }
+    return false
+  }
+
+  public readTargets(targets: string[]): string | null {
+    const char = this.regex[this.index]
+    const index = targets.indexOf(char)
+    return index === -1 ? null : targets[index]
+  }
+
+  advance(size: number): number {
+    return (this.index += size)
+  }
 }
 export default Lexer
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,3 +1,105 @@
-import Lexer from "./lexer"
-class Parser {}
+import { nanoid } from "nanoid"
+import * as AST from "./ast"
+import * as patterns from "./patterns"
+import Lexer, { TokenType } from "./lexer"
+
+class Parser {
+  regex: string
+  idGenerator: (size?: number) => string
+  flags: AST.Flag[] = []
+  message: string = ""
+  lexer!: Lexer
+  constructor(regex: string | RegExp, idGenerator = nanoid) {
+    if (typeof regex !== "string") {
+      regex = String(regex)
+    }
+    this.regex = regex
+    this.idGenerator = idGenerator
+  }
+
+  public parse(): AST.Regex | AST.RegexError {
+    let withSlash = true
+    if (this.regex.trim()[0] !== "/") {
+      this.regex = `/${this.regex}/`
+      withSlash = false
+    } else {
+      this.regex = this.regex.trim()
+    }
+
+    this.lexer = new Lexer(this.regex)
+
+    if (!this.validate()) {
+      return { type: "error", message: this.message }
+    }
+
+    this.lexer.read()
+    const body = this.parseNodes()
+    return {
+      type: "regex",
+      body,
+      flags: this.flags,
+      withSlash,
+    }
+  }
+
+  id() {
+    return this.idGenerator()
+  }
+
+  parseNodes(): AST.Node[] {
+    const nodes: AST.Node[] = []
+    while (true) {
+      const {
+        type,
+        span: { start, end },
+      } = this.lexer.read()
+      switch (type) {
+        case TokenType.GraphEnd | TokenType.RegexBodyEnd: {
+          return nodes
+        }
+        case TokenType.Literal: {
+          const value = this.regex.slice(start, end + 1)
+          const quantifier = this.parseQuantifier()
+          nodes.push({
+            id: this.id(),
+            type: "character",
+            kind: "string",
+            value,
+            quantifier,
+          })
+        }
+      }
+    }
+  }
+
+  parseQuantifier(): AST.Quantifier | null {
+    return null
+  }
+
+  validate() {
+    try {
+      let end = this.regex.lastIndexOf("/")
+      if (end <= 0) {
+        this.message = "Invalid regular expression"
+        return false
+      }
+
+      for (let i = end + 1; i < this.regex.length; i++) {
+        if (!patterns.flag.test(this.regex[i])) {
+          this.message = `Invalid regular expression flags '${this.regex[i]}'`
+          return false
+        }
+        this.flags.push(this.regex[i] as AST.Flag)
+      }
+
+      new RegExp(this.regex.slice(1, end), this.regex.slice(end + 1))
+    } catch (error) {
+      if (error instanceof Error) {
+        this.message = error.message
+      }
+      return false
+    }
+    return true
+  }
+}
 export default Parser
