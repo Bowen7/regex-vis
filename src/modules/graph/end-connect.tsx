7a0e99257d6e4dda5f49c9748e527531def1c7d0
Bowen7 <zwxdyx@foxmail.com>
2022-05-29 14:24:54 +0800

    refactor: end connect

--- src/modules/graph/end-connect.tsx
+++ src/modules/graph/end-connect.tsx
@@ -10,19 +10,19 @@ const EndConnect: React.FC<Props> = React.memo((props) => {
     return <MidConnect start={start} end={end} />
   }
   const M = `M${start[0]},${start[1]}`
-  const L1 = `L${start[0] + 10},${start[1]}`
+  const L1 = `L${end[0] - 20},${start[1]}`
   const L3 = `L${end[0]},${end[1]}`
   let A1 = ""
   let L2 = ""
   let A2 = ""
   if (end[1] > start[1]) {
-    A1 = `A5 5 0 0 1, ${start[0] + 15},${start[1] + 5}`
-    L2 = `L${start[0] + 15},${end[1] - 5}`
-    A2 = `A5 5 0 0 0, ${start[0] + 20},${end[1]}`
+    A1 = `A5 5 0 0 1, ${end[0] - 15},${start[1] + 5}`
+    L2 = `L${end[0] - 15},${end[1] - 5}` // end[0] - 25
+    A2 = `A5 5 0 0 0, ${end[0] - 10},${end[1]}` // end[0] - 20
   } else {
-    A1 = `A5 5 0 0 0, ${start[0] + 15},${start[1] - 5}`
-    L2 = `L${start[0] + 15},${end[1] + 5}`
-    A2 = `A5 5 0 0 1, ${start[0] + 20},${end[1]}`
+    A1 = `A5 5 0 0 0, ${end[0] - 15},${start[1] - 5}` // end[0] - 15
+    L2 = `L${end[0] - 15},${end[1] + 5}` // end[0] - 15
+    A2 = `A5 5 0 0 1, ${end[0] - 10},${end[1]}` // end[0] - 10
   }
   const path = M + L1 + A1 + L2 + A2 + L3
   return <path d={path} className="stroke" fill="none"></path>
