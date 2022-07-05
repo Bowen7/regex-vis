df0202c152265d5fcf5a1aa2cba4ae0582acc41a
Bowen7 <zwxdyx@foxmail.com>
2022-07-05 23:26:04 +0800

    withEscapedBackslashCharacterClass regex

--- src/parser/patterns.ts
+++ src/parser/patterns.ts
@@ -12,8 +12,8 @@ export const normalCharacterInLiteral = /[/()[|\\.^$?+*]|\{(\d+)(,|,(\d+))?\}/
 export const normalCharacter = /[()[|\\.^$?+*]|\{(\d+)(,|,(\d+))?\}/
 export const characterClass =
   /^\\(?:[dDwWsStrnvf0]|c[A-Za-z]|x[0-9A-Fa-f]{2}|u[0-9A-Fa-f]{4})/
-export const allowEscapedBackslashCharacterClass =
-  /^\\{1,2}(?:[dDwWsStrnvf0]|c[A-Za-z]|x[0-9A-Fa-f]{2}|u[0-9A-Fa-f]{4})/
+export const withEscapedBackslashCharacterClass =
+  /^(\\{1,2}(?:[trnvf0]||x[0-9A-Fa-f]{2}|u[0-9A-Fa-f]{4}))|(\\{2}(?:[dDwWsS]|c[A-Za-z]))/
 export const backReference = /^\\(\d+|k<(\w+)>)/
 export const withEscapedBackslashWordBoundary = /^\\\\[bB]/
 export const wordBoundary = /^\\[bB]/
