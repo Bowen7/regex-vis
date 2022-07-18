1f0bc0f94396ed3d6f2c4d663ca72141d2251ebb
Bowen <771797746@qq.com>
2022-07-18 23:04:40 +0800

    feat: add sentry (#53)

--- config-overrides.js
+++ config-overrides.js
@@ -6,6 +6,21 @@ const {
 } = require("customize-cra")
 const path = require("path")
 
+const findWebpackPlugin = (plugins, pluginName) =>
+  plugins.find((plugin) => plugin.constructor.name === pluginName)
+
+const overrideProcessEnv = (value) => (config) => {
+  const plugin = findWebpackPlugin(config.plugins, "DefinePlugin")
+  const processEnv = plugin.definitions["process.env"] || {}
+
+  plugin.definitions["process.env"] = {
+    ...processEnv,
+    ...value,
+  }
+
+  return config
+}
+
 module.exports = override(
   addWebpackAlias({
     "@": path.resolve(__dirname, "src"),
@@ -13,5 +28,8 @@ module.exports = override(
   addBabelPlugin("styled-jsx/babel"),
   fixBabelImports("@geist-ui/react", {
     libraryDirectory: "esm",
+  }),
+  overrideProcessEnv({
+    SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
   })
 )
