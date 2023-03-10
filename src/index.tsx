92cca6d894351024fca27ba5a5dce7bb2b232e4d
Bowen7 <zwxdyx@foxmail.com>
2023-03-10 20:08:21 +0800

    Add web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import * as Sentry from "@sentry/react"
 import { BrowserTracing } from "@sentry/tracing"
+import reportWebVitals from "./reportWebVitals"
+import { sendToVercelAnalytics } from "./vitals"
 import App from "./App"
 import "./i18n"
 import * as serviceWorker from "./serviceWorker"
@@ -21,6 +23,7 @@ root.render(
   </StrictMode>
 )
 
+reportWebVitals(sendToVercelAnalytics)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
