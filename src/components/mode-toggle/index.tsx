d195bf72ab16db0a8f790deeb0f117c9886a7b40
Bowen7 <zwxdyx@foxmail.com>
2024-08-16 23:42:18 +0800

    fix flash when changing the theme

--- src/components/mode-toggle/index.tsx
+++ src/components/mode-toggle/index.tsx
@@ -3,12 +3,15 @@ import { Button } from '@/components/ui/button'
 import { useTheme } from '@/components/theme-provider'
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  const onClick = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
 
   return (
-    <Button variant="outline" size="icon" className="h-8 w-8">
-      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" onClick={() => setTheme('dark')} />
-      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" onClick={() => setTheme('light')} />
+    <Button variant="outline" size="icon" className="h-8 w-8" onClick={onClick}>
+      {theme === 'dark' ? <MoonIcon className="absolute h-[1.2rem] w-[1.2rem]" /> : <SunIcon className="h-[1.2rem] w-[1.2rem]" />}
     </Button>
   )
 }
